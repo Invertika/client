3e26714541384ea2e0c0e96b21c5ecf57a58719e
seeseekey <seeseekey@gmail.com>
2012-08-20 15:47:37 +0200

    Error Enum hinzugefügt.

---
+++ invertika/classes/error.js
@@ -0,0 +1,16 @@
+var Error =
+{ 
+		// Generic return values
+		ERRMSG_OK=0,                      // everything is fine
+		ERRMSG_FAILURE=1,                     // the action failed
+		ERRMSG_NO_LOGIN=2,                    // the user is not yet logged
+		ERRMSG_NO_CHARACTER_SELECTED=3,     // the user needs a character
+		ERRMSG_INSUFFICIENT_RIGHTS=4,         // the user is not privileged
+		ERRMSG_INVALID_ARGUMENT=5,          // part of the received message was invalid
+		ERRMSG_EMAIL_ALREADY_EXISTS=6,       // The Email Address already exists
+		ERRMSG_ALREADY_TAKEN=7,             // name used was already taken
+		ERRMSG_SERVER_FULL=8,                // the server is overloaded
+		ERRMSG_TIME_OUT=9,                   // data failed to arrive in due time
+		ERRMSG_LIMIT_REACHED=10,            // limit reached
+		ERRMSG_ADMINISTRATIVE_LOGOFF=11       // kicked by server administrator
+}
