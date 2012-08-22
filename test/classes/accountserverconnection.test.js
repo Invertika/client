02b59b8da80715dad7734e1fe54008597781e721
seeseekey <seeseekey@gmail.com>
2012-08-22 15:47:27 +0200

    Tests überarbeitet.

--- test/classes/accountserverconnection.test.js
+++ test/classes/accountserverconnection.test.js
@@ -1,12 +1,22 @@
 AccountServerConnectionTest = (function() {
 	return {
-		connectAndSend: function() {
-			var result = 4;
-			Assert.areIdentical(5, result);
+		connect: function() {
+			var connection=new AccountServerConnection("echo.websocket.org", 80);
+			connection.login("test", "test");
+			
+			//Schlägt fehl wenn das Socket null ist
+			Assert.areNotIdentical(connection.socket, null);
 		},
-		connectAndSend2: function() {
-			var result = 4;
-			Assert.areIdentical(5, result);
+		
+		checkParameters: function() {
+			var connection=new AccountServerConnection("echo.websocket.org", 80);
+			connection.login("test", "secret");
+			
+			//Prüft ob die Parameter korrekt an die Klasse übergeben wurden
+			Assert.areIdentical(connection.username, "test");
+			Assert.areIdentical(connection.password, "secret");
+			Assert.areIdentical(connection.ip, "echo.websocket.org");
+			Assert.areIdentical(connection.port, 80);
 		}
 	};
 })();
