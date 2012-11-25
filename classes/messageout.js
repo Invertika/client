01fe1b66d25b1d61870450f5793535ad2170b571
seeseekey <seeseekey@gmail.com>
2012-11-25 21:25:11 +0100

    Beginn der Umstellung auf binäre Datenübertragung mittels Websockets.

--- classes/messageout.js
+++ classes/messageout.js
@@ -1,24 +1,61 @@
 var MessageOut = new Class({
 	id: 0,
 	parts: [],
+	data,
+	dataView,
+	length,
+	maxLength,
 	
 	//Konstruktor
 	initialize: function(id) {
-        this.id = id;
+        this.id = id;		
+		
+		this.length=0;
+		this.maxLength=1000;
+		
+		this.data=new ArrayBuffer(this.maxLength);
+		this.dataView=new DataView(this.data);
+		
+		//ID schreiben
+		this.dataView.setInt16(length, this.id);
+		length+=2;
     },
 	
 	//Methoden
-    addValue: function(value) {
-		if(typeOf(value)=='string')
+    addValueAsInt8: function(value) {
+		//if(typeOf(value)=='string')
+		//{
+		//	var maskedValue = value.replace(/:/g, "::");
+		//	this.parts.include(maskedValue);
+		//}
+		//else
+		//{
+		//	this.parts.include(value);
+		//}
+		
+		//this.parts.include(value);
+		
+		if((this.length+1)>=this.maxLength)
 		{
-			var maskedValue = value.replace(/:/g, "::");
-			this.parts.include(maskedValue);
+			log.error('Maximum byte count for MessageOut is reached.');
+			return;
 		}
-		else
-		{
-			this.parts.include(value);
-		}
+		
+		this.dataView.setInt8(length, value);
+		length+=1;
     },
+	
+	getBinary: function() {
+		var ret=this.id.toString(16)+":";
+		var length=this.parts.length;
+
+		this.parts.each(function(part, index){
+			if(index == length-1) ret+=part;
+			else ret+=part+":";
+		});
+		
+		return ret;
+	},
 	
 	getString: function() {
 		var ret=this.id.toString(16)+":";
--- index.js
+++ index.js
@@ -56,8 +56,18 @@ uki('Label').click(function() {
 //Debug
 function debug()
 {
-	accountServer=new AccountServerConnection(ip, 9601);
-	accountServer.login("florian", "geheim");
+	//accountServer=new AccountServerConnection(ip, 9601);
+	//accountServer.login("florian", "geheim");
+	
+	var registerMsg=new MessageOut(Protocol.PAMSG_REGISTER);
+	registerMsg.addValue(PROTOCOL_VERSION); //Client Version
+	registerMsg.addValue(username);
+	registerMsg.addValue(sha256_digest(username + password)); // Use a hashed password for privacy reasons
+	registerMsg.addValue(email);
+	registerMsg.addValue(captchaResponse);
+	
+	var blob=registerMsg.getBinary();
+	alert(blob);
 	
 	//login("seeseekey", "geheim");
 	//login("schnee", "geheim");
